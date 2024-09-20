f9d0dd538a0a9a3a81b5b836e7b872a3743137a6
mark <piringerm@kkszki.hu>
2024-09-20 13:08:56 +0200

    the result is comleted

--- Program.cs
+++ Program.cs
@@ -16,9 +16,28 @@ namespace terület
             int b;
             int T;
 
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+
+                Console.Write("kérem 'a' oldal értékét(0> kell lennie)");
+                a = Convert.ToInt32(Console.ReadLine());
+
+            } while (a <= 0);
+
+            do
+            {
+
+                Console.Write("kérem 'b' oldal értékét(0> kell lennie)");
+                b = Convert.ToInt32(Console.ReadLine());
+
+            } while (b <= 0);
+
             T = a * b;
+
+            Console.WriteLine($"a tégla lap területe:{T}");
+
+            Console.ReadLine();
         }
         static void Main(string[] args)
         {
